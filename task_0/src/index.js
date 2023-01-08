609bbefed92d912746364994d378922b609d8dc1
AJS0191 <ashtonjamess@live.com>
2023-01-08 11:52:02 -0600

    adding task_0

---
+++ task_0/src/index.js
@@ -0,0 +1,8 @@
+import $ from "jquery";
+console.log('TEST SUCCESS')
+
+$( document ).ready(function() {
+  $("body").append("<p>Holberton Dashboard</p>");
+  $("body").append("<p>Dashboard data for the students</p>");
+  $("body").append("<p>Copyright - Holberton School</p>");
+})

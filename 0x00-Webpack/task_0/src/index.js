2feadc366fcb32136a04032ab208256e3a429823
AJS0191 <ashtonjamess@live.com>
2023-01-08 12:22:03 -0600

    update package, single quotes

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -2,7 +2,7 @@ import $ from "jquery";
 console.log('TEST SUCCESS')
 
 $( document ).ready(function() {
-  $("body").append("<p>Holberton Dashboard</p>");
-  $("body").append("<p>Dashboard data for the students</p>");
-  $("body").append("<p>Copyright - Holberton School</p>");
+  $('body').append("<p>Holberton Dashboard</p>");
+  $('body').append("<p>Dashboard data for the students</p>");
+  $('body').append("<p>Copyright - Holberton School</p>");
 })

545b0629ead92664c7e9a6abe0738474177f2091
AJS0191 <ashtonjamess@live.com>
2023-01-10 16:44:34 -0600

    add task 2

---
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -0,0 +1,23 @@
+import $ from "jquery";
+import _ from 'lodash';
+
+
+$( document ).ready(function() {
+  $('body').append("<div id=logo></div>");
+  $('body').append("<p>Holberton Dashboard</p>");
+  $('body').append("<p>Dashboard data for the students</p>");
+  $('body').append("<button>Click here to get started</button>");
+  $('body').append("<p id='count'></p>");
+  $('body').append("<p>Copyright - Holberton School</p>");
+  delayUpdateCounter()
+})
+
+function updateCounter() {
+  let count = 0;
+  $('#count').click(function() {
+    count = count + 1
+    this.text(`{count} clicks on the button`)
+  })
+}
+
+const delayUpdateCounter = _.debounce(updateCounter, 500);

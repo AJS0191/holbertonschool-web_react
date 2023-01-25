0a62340e57ed6b60d5e3cc9f6b0ad3a0d2fb1be7
AJS0191 <ashtonjamess@live.com>
2023-01-25 12:15:13 -0600

    useing mer and concat on lists and maps

--- 0x01-React_Immutable/5-merge.js
+++ 0x01-React_Immutable/5-merge.js
@@ -1,11 +1,11 @@
 import Immutable, {Map, List} from 'immutable';
 
 function concatElements(page1, page2){
-return List.concat(page1, page2)
+return List(page1).concat(List(page2))
 }
 
 function mergeElements(page1, page2){
-  return Map.mergeWith(page1, page2)
+  return Map(page1).merge(Map(page2))
 }
 
 export {mergeElements, concatElements};

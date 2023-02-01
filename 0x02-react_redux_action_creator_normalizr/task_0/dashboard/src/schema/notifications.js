9183a4c92b4df04f79aed96759c1b677d27277f2
AJS0191 <ashtonjamess@live.com>
2023-02-01 13:55:32 -0600

    update function with for loop

--- 0x02-react_redux_action_creator_normalizr/task_0/dashboard/src/schema/notifications.js
+++ 0x02-react_redux_action_creator_normalizr/task_0/dashboard/src/schema/notifications.js
@@ -1,13 +1,15 @@
 const notifList = require('../../../../notifications.json')
 
-export function getAllNotificationsByUser(userId) {
-  const  matchList = []
-  for (notification in notifList) {
-    if (userId === notification.author.id) {
-      matchList.push(notification)
+function getAllNotificationsByUser(userId) {
+  let notifications = [];
+  for (let i in notifList) {
+    if (notifList[i].author.id === userId) {
+      notifications.push(notifList[i].context);
     }
   }
-  return matchList
+  return notifications;
 }
+
+console.log(getAllNotificationsByUser('5debd764a7c57c7839d722e9'))
 
 

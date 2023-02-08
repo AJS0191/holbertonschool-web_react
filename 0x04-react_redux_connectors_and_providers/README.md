4e96645b80727b3bbd80e7b99914ccffe3ad190a
AJS0191 <ashtonjamess@live.com>
2023-02-08 10:45:02 -0600

    adding README, setting up connect in App.js

---
+++ 0x04-react_redux_connectors_and_providers/README.md
@@ -0,0 +1,2 @@
+# holbertonschool-web_react
+In this directory we will be finishing our react projects by rounding out our knowledge of redux. 
---
+++ 0x04-react_redux_connectors_and_providers/task_0/dashboard/src/schema/courses.js
@@ -0,0 +1,9 @@
+import { schema, normalize} from 'normalizr'
+
+const coursesSchema = new schema.Entity('courses');
+
+function coursesNormalizer(data) {
+  return normalize(data, [coursesSchema]);
+}
+
+module.exports = coursesNormalizer;
